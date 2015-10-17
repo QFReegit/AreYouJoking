6461e4d0d030378941dc7a5649eee1094e0f235f
QFReegit <qfreegit@mail.ru>
2015-10-17 14:14:40 +0300

    initial commit

---
+++ AreYouJoking/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AreYouJoking
+//
+//  Created by Егор Данилов on 17.10.15.
+//  Copyright © 2015 Egor Danilov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
